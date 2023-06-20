62152b8062987813c0b14429c0f838a4a984cb27
Maximilian Schmidt <maximilian@schmidt.so>
2023-06-20 13:20:32 +0200

    Skip cells, which are not 3x3

--- flow.py
+++ flow.py
@@ -8,12 +8,13 @@ from tqdm import tqdm
 import multiprocessing
 
 
-ROOT_DIR = Path("/hkfs/work/workspace/scratch/hgf_pdv3669-health_train_data/train")
-ROOT_DIR = Path("../")
+ROOT_DIR = Path("/hkfs/work/workspace/scratch/hgf_pdv3669-H3/train/")
 
 
 def simulate_heat_diffusion(indices, heat_center):
     index_range = np.max(indices, axis=0) - np.min(indices, axis=0) + 1
+    if index_range[0] < 3 or index_range[1] < 3:
+        return None
     heat_map = np.zeros(index_range)
     heat_source = heat_center - np.min(indices, axis=0)
     heat_indices = indices - np.min(indices, axis=0)
@@ -60,7 +61,11 @@ def compute_gradient_mask(mask_path):
         distances = np.sum((indices - median) ** 2, axis=1)
         center = indices[np.argmin(distances)]
 
-        gradients, heat_indices = simulate_heat_diffusion(indices, center)
+        result = simulate_heat_diffusion(indices, center)
+        if result is None:
+            progress.write(f"In {mask_path.name}: skipping ID {id} not large enough")
+            continue
+        gradients, heat_indices = result
         normalize_gradients(gradients)
 
         mask_gradients[:, indices[:, 0], indices[:, 1]] = gradients[
@@ -83,7 +88,7 @@ def compute_gradient_masks(mask_path):
 if __name__ == "__main__":
     mask_files = sorted(list(ROOT_DIR.glob(r"[abc]" + "_GT/man_*.tif")))
     # progress = tqdm(mask_files, desc="Mask Files")
-    with Pool(2) as p:
+    with Pool(15) as p:
         for _ in tqdm(
             p.imap_unordered(compute_gradient_masks, mask_files),
             total=len(mask_files),
