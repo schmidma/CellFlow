df6ee573da6d9f69bd9074546647bb13443d0f02
Jonathan Hellwig <jonathan.hellwig@posteo.de>
2023-06-20 14:15:45 +0200

    Update cell dataset

--- dataset.py
+++ dataset.py
@@ -4,6 +4,7 @@ import albumentations
 from albumentations.pytorch.transforms import ToTensorV2
 import numpy as np
 import tifffile
+import torch
 from torch.utils.data import Dataset
 
 
@@ -13,44 +14,43 @@ class CellDataset(Dataset):
 
         root_dir = Path(root_dir)
         if split == "train":
-            self.img_files = sorted(list(root_dir.glob(r"[ab]" + "/*.tif")))
-            self.mask_files = sorted(list(root_dir.glob(r"[ab]" + "_GT/*.tif")))
+            self.image_files = sorted(list(root_dir.glob(r"[ab]" + "/*.tif")))
+            self.flow_gradient_files = sorted(
+                list(root_dir.glob(r"[ab]" + "_flow/*.tif"))
+            )
         elif split == "val":
-            self.img_files = sorted(list(root_dir.glob(r"[c]" + "/*.tif")))
-            self.mask_files = sorted(list(root_dir.glob(r"[c]" + "_GT/*.tif")))
+            self.image_files = sorted(list(root_dir.glob(r"[c]" + "/*.tif")))
+            self.flow_gradient_files = sorted(
+                list(root_dir.glob(r"[c]" + "_flow/*.tif"))
+            )
         elif split == "test":
-            self.img_files = sorted(list(root_dir.glob(r"[de]" + "/*.tif")))
-            self.mask_files = None
+            self.image_files = sorted(list(root_dir.glob(r"[de]" + "/*.tif")))
+            self.flow_gradient_files = None
 
     def __getitem__(self, idx):
-        img = tifffile.imread(self.img_files[idx])
-        mask = (
-            tifffile.imread(self.mask_files[idx]).astype(np.float32)
-            if self.mask_files
-            else None
-        )
+        image = tifffile.imread(self.image_files[idx])
+        if self.flow_gradient_files:
+            flow_gradient = torch.tensor(
+                tifffile.imread(self.flow_gradient_files[idx]).astype(np.float32)
+            )
+        else:
+            flow_gradient = torch.zeros((2, image.shape[0], image.shape[1]))
 
         if self.transform is not None:
-            if self.mask_files:
-                transformed = self.transform(image=img, mask=mask)
-            else:
-                transformed = self.transform(image=img)
-            img = transformed["image"]
-            if self.mask_files:
-                mask = transformed["mask"].long()
-
-        return img, mask if mask is not None else 1
+            transformed_image = self.transform(image=image)["image"]
+        mask = torch.sum(flow_gradient**2, axis=0) != 0
+        return transformed_image, mask, flow_gradient
 
     def __len__(self):
-        return len(self.img_files)
+        return len(self.image_files)
 
 
 def train_transform():
     transform = albumentations.Compose(
         [
             # albumentations.Resize(512, 512, interpolation=cv2.INTER_NEAREST),
-            albumentations.HorizontalFlip(p=0.5),
-            albumentations.VerticalFlip(p=0.5),
+            # albumentations.HorizontalFlip(p=0.5),
+            # albumentations.VerticalFlip(p=0.5),
             # albumentations.RandomBrightnessContrast(p=0.2),
             albumentations.Normalize(
                 mean=33.53029578908284 / 255,
@@ -76,3 +76,6 @@ def val_transform():
     )
 
     return transform
+
+if __name__ == "__main__":
+    cell_dataset = CellDataset()
