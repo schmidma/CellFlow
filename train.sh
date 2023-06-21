d395fb6512c99c29e19962a8d9b31bc762461bf2
Maximilian Schmidt <maximilian@schmidt.so>
2023-06-21 09:27:17 +0200

    srun python

--- train.sh
+++ train.sh
@@ -4,16 +4,17 @@
 #SBATCH --partition=accelerated
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=152
 #SBATCH --time=00:05:00
 #SBATCH --output=/hkfs/work/workspace/scratch/hgf_pdv3669-H3/training.txt
 
 export CUDA_CACHE_DISABLE=1
 export OMP_NUM_THREADS=1
+export NCCL_DEBUG=INFO
 
 group_workspace=/hkfs/work/workspace/scratch/hgf_pdv3669-H3
 
 source ${group_workspace}/venv/bin/activate
-python ${group_workspace}/CharmingSyringes/train.py --root_dir ${group_workspace}/
+srun python ${group_workspace}/CharmingSyringes/train.py --root_dir ${group_workspace}/
 
